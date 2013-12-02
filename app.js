351b9b660815cc2b087a910d17f5656501102420
OpenShift guest <dten1986@mail.ru>
2013-12-02 08:48:25 -0500

    developing...

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+
+var fs = require('fs');
+	    
+
 var app = express();
 
 // port
@@ -34,7 +38,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+module.exports = function(app) {
+//  app.get('/handler', require('./handler').get);
+  app.post('/handler', require('./handler').post);
 };
